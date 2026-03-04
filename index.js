2ac00f921d8d99603ea1143b45d963e76a7771fb
muhammadafzalsiddiqui <khanafzal155@gmail.com>
2026-03-05 02:17:56 +0500

    sing up with fire base complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+import { getAuth , createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
   const firebaseConfig = {
     apiKey: "AIzaSyAynJ89FG2n-RpUqfTtKFt9DunfyXiYBKI",
     authDomain: "quote-app-25d69.firebaseapp.com",
@@ -14,4 +14,33 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-aut
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app)
   console.log("auth=>",auth)
+ //sign up
+
+
+var btn = document.querySelector(".login-btn");
+
+btn.addEventListener("click", signup);
+
+function signup() {
+  var eemail = document.getElementById("semail").value;
+  var epassword = document.getElementById("spassword").value;
+
+
+createUserWithEmailAndPassword(auth, eemail, epassword)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+}
+ 
+
+
+
+
 
